c58752e34a44930d3a1128a2774684b71631bc2c
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2020-11-17 07:26:09 -0300

    feat: changes rental model

--- models/rental.js
+++ models/rental.js
@@ -1,21 +1,67 @@
 const mongoose = require("mongoose");
 const joi = require("joi");
-const { movieSchema } = require("./movie");
+
+const rentalSchema = new mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 10,
+      },
+      isGold: {
+        type: Boolean,
+        default: false,
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+      },
+    }),
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        maxlength: 255,
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255,
+      },
+    }),
+    required: true,
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  dateReturned: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 0,
+  },
+});
 
 function validateRentals(rental) {
   const clientSchema = {
     movieId: joi.string().required(),
+    customerId: joi.string().required(),
   };
 
   return joi.validate(rental, clientSchema);
 }
-
-const rentalSchema = new mongoose.Schema({
-  movie: {
-    type: movieSchema,
-    required: true,
-  },
-});
 
 const Rental = new mongoose.model("rental", rentalSchema);
 
--- routes/rentals.js
+++ routes/rentals.js
@@ -1,7 +1,8 @@
-const router = require("express").Router();
-const mongoose = require("mongoose");
 const { Movie } = require("../models/movie");
+const { Customer } = require("../models/customer");
 const { Rental, validate } = require("../models/rental");
+
+const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   const rentals = await Rental.find();
@@ -12,19 +13,34 @@ router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) res.status(400).send(error.details[0].message);
 
-  const movie = await Movie.findById(req.body.movieId);
-  if (!movie) res.send("No rentals of the given movieId were found");
+  const { movieId, customerId } = req.body;
+
+  const customer = await Customer.findById(customerId);
+  if (!customer) return res.status(400).send("Invalid customer.");
+
+  const movie = await Movie.findById(movieId);
+  if (!movie) res.send("Invalid movie");
+
+  if (movie.numberInStock === 0)
+    return res.status(400).send("Movie not in stock.");
 
   let rental = new Rental({
+    customer: {
+      _id: customer._id,
+      name: customer.name,
+      phone: customer.phone,
+    },
     movie: {
+      _id: movie._id,
       title: movie.title,
-      genre: movie.genre,
-      numberInStock: movie.numberInStock,
       dailyRentalRate: movie.dailyRentalRate,
     },
   });
-
   rental = await rental.save();
+
+  movie.numberInStock--;
+  movie.save();
+
   res.send(rental);
 });
 
