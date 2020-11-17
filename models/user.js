2b9a1c3d80b3cefbdadd2f8d1ebd84cac2f2a331
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2020-11-17 08:47:46 -0300

    feat: adds verification for user email before saving new user

--- models/user.js
+++ models/user.js
@@ -1,21 +1,27 @@
 const mongoose = require("mongoose");
 const joi = require("joi");
 
+const userSchema = new mongoose.Schema({
+  name: { type: String, minlength: 5, maxlength: 50, required: true },
+  email: {
+    type: String,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+    required: true,
+  },
+  password: { type: String, minlength: 5, maxlength: 1024, required: true },
+});
+
 function validateUser(user) {
   const schema = {
-    name: joi.string().min(2).required(),
-    email: joi.string().email().required(),
-    password: joi.string().min(4).max(18).required(),
+    name: joi.string().min(5).max(50).required(),
+    email: joi.string().min(5).max(255).email().required(),
+    password: joi.string().min(5).max(255).required(),
   };
 
   return joi.validate(user, schema);
 }
-
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String, minlength: 4, maxlength: 18, required: true },
-});
 
 const User = new mongoose.model("user", userSchema);
 
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,5 @@
 const { User, validate } = require("../models/user");
-
 const router = require("express").Router();
-const mongoose = require("mongoose");
 
 router.get("/", async (req, res) => {
   const users = await User.find();
@@ -13,11 +11,16 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   const { name, email, password } = req.body;
-  const newUser = new User({
+
+  let newUser = await User.findOne({ email });
+  if (newUser) return res.status(400).send("User already registred.");
+
+  newUser = new User({
     name,
     email,
     password,
   });
+
   try {
     await newUser.save();
     res.send(newUser);
