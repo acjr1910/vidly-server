8c3a348bc482efed87d9f129684d9829bbffb23f
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2020-11-16 21:42:46 -0300

    feat: adds rental api

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 
 const mongooseOptions = {
   useNewUrlParser: true,
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
