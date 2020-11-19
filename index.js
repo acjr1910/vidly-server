45fb7e5e8f14097cfd45d40960edca54a00a82e6
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2020-11-19 18:00:09 -0300

    chore: extracting routes from index

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ require('express-async-errors')
 const winston = require('winston')
 require('winston-mongodb')
 const config = require('config')
-const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
 const joi = require('joi')
 joi.objectId = require('joi-objectid')(joi)
 
-const genres = require('./routes/genres')
-const customers = require('./routes/customers')
-const movies = require('./routes/movies')
-const rentals = require('./routes/rentals')
-const users = require('./routes/users')
-const auth = require('./routes/auth')
+const express = require('express')
+const app = express()
 
-const error = require('./middleware/error')
+require('./startup/routes')(app)
+
+winston.exceptions.handle(
+  new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+)
+
+process.on('unhandledRejection', (ex) => {
+  throw ex
+})
 
 winston.add(new winston.transports.File({ filename: 'logfile.log' }))
 winston.add(
@@ -39,16 +41,6 @@ mongoose
   .connect('mongodb://localhost/vidly', mongooseOptions)
   .then(() => console.log('Connected to MongoDB...'))
   .catch((error) => console.log('Could not connect to MongoDB', error))
-
-app.use(express.json())
-app.use('/api/genres', genres)
-app.use('/api/customers', customers)
-app.use('/api/movies', movies)
-app.use('/api/rentals', rentals)
-app.use('/api/users', users)
-app.use('/api/auth', auth)
-
-app.use(error)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
